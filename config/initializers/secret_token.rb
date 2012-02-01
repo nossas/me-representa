4272d28b2aa482a76e86058f5e7d6e3b59b6a957
Luiz Fonseca <runeroniek@gmail.com>
2012-02-01 17:29:31 -0200

    First commit - configure initial gems and ignore some files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CariocaPergunta::Application.config.secret_token = 'd14dac544a14601a1075517a1b72472c5322416a94bccf5a9523122bf507eb612c53f48e7d5095b59cda4b2e7a7a4402ba2c5e4d25a288136e549a0fb7e61a42'
