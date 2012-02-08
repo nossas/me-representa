1ce486f8ecf3d3a4d8105440574575e227ad6006
Luiz Fonseca <runeroniek@gmail.com>
2012-02-08 16:35:22 -0200

    Add compass rules for compiling on heroku

--- config/compass.rb
+++ config/compass.rb
@@ -2,7 +2,7 @@ require "ninesixty"
 
 project_type = :rails
 
-css_dir = "public/stylesheets"
+css_dir = (environment == :development) ? "public/stylesheets" : "tmp/stylesheets"
 sass_dir = "app/assets/stylesheets"
 
 output_style = (environment == :production) ? :compressed : :expanded
