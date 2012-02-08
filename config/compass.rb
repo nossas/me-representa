95a7b733b2ec2e933ee900f31b3ec7a8e02ce653
Luiz Fonseca <runeroniek@gmail.com>
2012-02-08 16:54:12 -0200

    Rollback configuration of compass

--- config/compass.rb
+++ config/compass.rb
@@ -2,7 +2,7 @@ require "ninesixty"
 
 project_type = :rails
 
-css_dir = (environment == :production) ? "tmp/stylesheets" : "public/stylesheets"
+css_dir = "public/stylesheets"
 sass_dir = "app/assets/stylesheets"
 
 output_style = (environment == :production) ? :compressed : :expanded
