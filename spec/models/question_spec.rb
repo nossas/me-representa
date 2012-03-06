37ee0e1077317efcce30faf7e734c9031c6a6a17
Luiz Fonseca <runeroniek@gmail.com>
2012-03-06 17:58:06 -0300

    Fix spec for Travis

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,9 +4,5 @@ describe Question do
   describe "Validations/Associations" do
     it { should belong_to :category }
     it { should belong_to :user }
-    it { should validate_presence_of :text }
-    it { should validate_presence_of :role_type }
-    it { should validate_presence_of :category }
-    it { should validate_presence_of :user }
   end
 end
