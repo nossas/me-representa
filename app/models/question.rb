b89fd0b2e40055886ff7fde18e9e67add35e5d31
Luiz Fonseca <runeroniek@gmail.com>
2012-03-06 17:49:06 -0300

    Changes.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,16 +2,8 @@ class Question < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   validates_presence_of :text, :role_type, :category, :user
-
+  validates_inclusion_of :role_type, :in => %Q{truth dare}
 
   scope :truth, where(['role_type = ?', 'truth'])
   scope :dare, where(['role_type = ?', 'dare'])
-
-  def role_type=(role_type)
-    if role_type == "truth" or role_type == "dare"
-      write_attribute(:role_type, role_type)
-    else
-      errors.add(:role_type, "Should be truth or dare")
-    end
-  end
 end
