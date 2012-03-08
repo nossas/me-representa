fbb9e86bd3fc963e46b791bca083c1014656ef18
Luiz Fonseca <runeroniek@gmail.com>
2012-03-08 18:02:44 -0300

    Change to the factory girl new syntax.

---
+++ spec/factories.rb
@@ -0,0 +1,26 @@
+FactoryGirl.define do
+
+  sequence(:name) { |s| "User #{s}" }
+  sequence(:email) { |s| "user#{s}@email.com" }
+
+  factory(:user) do |user|
+    user.email FactoryGirl.generate(:email)
+  end
+
+  factory(:category) do |c|
+    c.name "Name"
+  end
+
+
+
+  factory(:question) do |q|
+    q.text "Text"
+    q.role_type "truth"
+    q.association :user
+    q.association :category
+  end
+end
+
+
+
+
