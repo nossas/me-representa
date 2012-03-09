bce2a83d99e30dc4657b87e0f0385ed8727aa81d
Luiz Fonseca <runeroniek@gmail.com>
2012-03-09 17:50:41 -0300

    Change routes, now root is Subscriber.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ CariocaPergunta::Application.routes.draw do
   match '/auth/:provider/callback', :to => 'sessions#create'
   resources :questions
   resources :users, :only => [:new, :create]
-
+  resources :subscribers, :only => [:create]
   # Remove the default if you're running the app and not the splash page :)
-  root :to => "users#index", :defaults => { :is_splash => true }
+  root :to => "subscribers#index", :defaults => { :is_splash => true }
 end
