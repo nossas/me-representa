4475366d25483946f38b3de520e63e21f0a1c2f1
Nícolas Iensen <nicolas@quavio.com.br>
2012-03-09 15:49:38 -0300

    Add omniauth

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'omniauth'
+gem 'omniauth-oauth2'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CariocaPergunta::Application.routes.draw do
+  match '/auth/:provider/callback', :to => 'sessions#create'
   post '/users/' => "users#new"
   root :to => "users#index"
 end
