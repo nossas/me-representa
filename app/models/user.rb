571ebe6bef6db95947967253fd4a09ad515485a2
Luiz Fonseca <runeroniek@gmail.com>
2012-03-11 16:42:30 -0300

    Fix wrong attributes hash.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   has_many :questions
   has_many :authorizations
-  validates_presence_of :email
+  validates_presence_of :email, :name
 
   def self.create_from_hash!(hash)
-    create(:email => hash['info']['email'], :name => hash['info']['first_name'], :picture => hash['info']['image_url'])
+    create(
+          :email => hash['email'],
+          :name => "#{hash['first_name']} #{hash['last_name']}",
+          :picture => hash['image_url']
+          )
   end
 end
