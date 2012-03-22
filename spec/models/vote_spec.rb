c8ddb5b3b8cfaf84f6cf77e33ce861f7726fc1f7
Luiz Fonseca <runeroniek@gmail.com>
2012-03-21 22:04:08 -0300

    Fix specs.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe Vote do
+  before do
+    Factory(:vote)
+  end
   describe "Associations/Validations" do
     it { should belong_to :question }
     it { should belong_to :user }
-    it { should validate_presence_of :user }
+    it { should validate_presence_of :question }
   end
 end
