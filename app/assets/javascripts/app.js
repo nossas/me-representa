3ad26f01bb903f221217aa2f4a946cef21c75bcb
Nícolas Iensen <nicolas@quavio.com.br>
2012-03-23 19:13:11 -0300

    Question share working after post

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,6 @@
 var App = window.App = {
   Common: {
     init: function(){
-      $(".questions_list .share").css({ 'opacity' : 0.4 });;
-      $("li").mouseover(function(){ $(this).find(".share").css({ 'opacity' : 1 }); });
-      $("li").mouseout(function(){ $(this).find(".share").css({ 'opacity' : 0.4 }); });
       App.Common.login = new App.Common.Login({el: $('section.login')[0]});
     },
 
