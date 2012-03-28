7d4acca29dcffad7f28f08040ca722fc959e4c75
Diogo Biazus <diogob@gmail.com>
2012-03-28 10:53:14 -0300

    removed the more action, lets use the index

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,11 +3,6 @@ require 'spec_helper'
 describe QuestionsController do
   subject{ response }
 
-  describe "GET 'more'" do
-    before{ get :more }
-    it{ should be_success }
-  end
-
   describe "GET 'index'" do
     before{ get :index }
     it{ should be_success }
