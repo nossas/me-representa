85a1e4b032d0cd85533d59aaad2501f57b794439
Luiz Fonseca <runeroniek@gmail.com>
2012-08-16 15:07:36 -0300

    Add MeuRio topbar (standard) and migrate some layout files.

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ CariocaPergunta::Application.routes.draw do
   resources :questions do
     resources :votes, :only => :create
   end
-  resources :users,       :only => [:new, :create]
+  resources :users,       only: [:new, :create]
   resources :subscribers
-  resources :sessions,    :only => [:destroy]
+  resources :sessions,    only: [:destroy]
   
-  get 'auth/meurio', as: :meurio_auth
-  get 'auth/facebook', as: :facebook_auth
+  get 'auth/meurio',      as: :meurio_auth
+  get 'auth/facebook',    as: :facebook_auth
 
   root :to => "candidates#index"
 end
