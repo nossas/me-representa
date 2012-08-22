c686a1b5ccf9b90f9be652da5619ebe435674901
Nícolas Iensen <nicolas@engage.is>
2012-08-22 15:17:58 -0300

    Add token to candidate and set candidate model validations and filters

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,3 +1,6 @@
 class Candidate < ActiveRecord::Base
   attr_accessible :born_at, :male, :name, :nickname, :number, :party_id
+  validates :name, :nickname, :number, :token, :uniqueness => true
+  validates :name, :nickname, :number, :party_id, :presence => true
+  before_create { self.token = Digest::SHA1.hexdigest Time.now.to_s }
 end
