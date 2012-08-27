047ad5e4991fa8bb5e248073ac4708b1f8fc170d
Luiz Fonseca <runeroniek@gmail.com>
2012-08-27 16:06:01 -0300

    Fix test.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 describe UsersController do
   describe "#index" do
-    before do
-      get :index, format: :csv
-    end
+#    context "When not an admin user" do
+      #before do
+        #get :index, format: :csv
+      #end
+      #its(:status) { should_not be_== 200 }
+    #end
 
-    its(:status) { should be_== 200 }
+    context "When an admin user" do
+      before do 
+        user = FactoryGirl.create(:user, admin: true)  
+        controller.stub!(:current_user).and_return(user)
+        get :index, format: :csv
+      end
+      its(:status) { should be_== 200 }
+    end
   end
 end
