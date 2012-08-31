82648aa4fcca1dd1a9a46a281d4e235902c7a224
Nícolas Iensen <nicolas@engage.is>
2012-08-31 14:16:18 -0300

    Bio should not be mandatory

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,7 +1,7 @@
 class Candidate < ActiveRecord::Base
   attr_accessible :born_at, :male, :name, :nickname, :number, :party_id, :party, :email, :mobile_phone, :bio
   validates :number, :token, :uniqueness => true
-  validates :name, :number, :party_id, :bio, :presence => true
+  validates :name, :number, :party_id, :presence => true
   belongs_to :party
   has_many :answers, :as => :responder
   before_create { self.token = Digest::SHA1.hexdigest Time.now.to_s }
