34c71edf666e18a5f6b9443d9d655945c7dbc28c
Luiz Fonseca <runeroniek@gmail.com>
2012-09-05 16:06:29 -0300

    Little fixes for candidates and implements Meurio notifications based on candidates input on candidates resend url form. #391211

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,9 +1,10 @@
 # coding: utf-8
 class CandidatesController < ApplicationController
   inherit_resources
-  respond_to :csv
   layout "application_phase_two"
   load_and_authorize_resource
+  skip_authorize_resource :only => :check  
+
   before_filter :only => [:index] {@truths = Question.truths.chosen}
   before_filter :only => [:index] {@dares = Question.dares.chosen}
   before_filter :only => [:check] { render json: nil if params[:candidate][:email] == "" and params[:candidate][:mobile_phone] == "" }
@@ -18,7 +19,10 @@ class CandidatesController < ApplicationController
   def check
     candidate = params[:candidate]
     @candidate = Candidate.find_by_email(candidate[:email]) || Candidate.find_by_mobile_phone(candidate[:mobile_phone])
-    result = { email: (@candidate.email == candidate[:email]), mobile_phone: (@candidate.mobile_phone == candidate[:mobile_phone]) }
+    result = { 
+      email: (@candidate.present? ? candidate[:email] == @candidate.email : false), 
+      mobile_phone: (@candidate.present? ? candidate[:mobile_phone] == @candidate.mobile_phone : false) 
+    }
     respond_to do |format|
       if @candidate.nil?
         format.json { render json: nil }
@@ -27,7 +31,9 @@ class CandidatesController < ApplicationController
           render json: result 
         }
 
-        CandidateMailer.resend_unique_url(@candidate).deliver if result[:email] == true
+        CandidateMailer.resend_unique_url(@candidate).deliver             if result[:email] == true
+        CandidateMailer.notify_meurio(@candidate).deliver                 if result[:mobile_phone] == true  and result[:email] == false
+        CandidateMailer.resend_unique_url_and_notify(@candidate).deliver  if result[:email] == true         and result[:mobile_phone] == true
       end
     end
   end
