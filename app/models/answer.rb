39760fd7abf11df199d5b6ee70eaca0b4d19cd68
Nícolas Iensen <nicolas@engage.is>
2012-09-13 18:28:42 -0300

    #391153 add weight to the answer form

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
-  attr_accessible :long_answer, :question_id, :responder_id, :responder_type, :short_answer, :question, :responder
+  attr_accessible :long_answer, :question_id, :responder_id, :responder_type, :short_answer, :question, :responder, :weight
   belongs_to :responder, :polymorphic => true
   belongs_to :question
   validates :question_id, :responder_id, :short_answer, :presence => true
