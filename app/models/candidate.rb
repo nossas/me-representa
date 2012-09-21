394bf14aa8a452e37370d2155b9a41d75d0d2420
Luiz Fonseca <runeroniek@gmail.com>
2012-09-21 18:59:28 -0300

    Indentation & Cosmetics

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -6,8 +6,8 @@ class Candidate < ActiveRecord::Base
   has_many :answers, :as => :responder
   before_create { self.token = Digest::SHA1.hexdigest("#{Time.now.to_s}#{self.number}") }
  
-  scope :by_age,          ->(start, end_at)         { where(['EXTRACT(year from current_date) - EXTRACT(year from born_at) 
-                                                     BETWEEN ? AND ?', start, end_at]) }
+  scope :by_age,          ->(start, end_at) { where(['EXTRACT(year from current_date) - EXTRACT(year from born_at) 
+                                                      BETWEEN ? AND ?', start, end_at]) }
   scope :by_gender,       ->(*genders)      { where(male: genders)            } 
   scope :by_scholarity,   ->(*scholarities) { where(scholarity: scholarities) }
   scope :by_reelection,   ->(*politicians)  { where(politician: politicians)  }
@@ -27,14 +27,19 @@ class Candidate < ActiveRecord::Base
   def self.match_for_user user_id, options = {:party_id => nil, :union_id => nil}
     candidates = 
       Candidate.
-      select("candidates.id, parties.symbol as symbol, name, round(sum((CASE WHEN answers.short_answer = ua.short_answer THEN 100 WHEN ua.short_answer IS NULL THEN NULL ELSE 0 END)::numeric * ua.weight)::numeric / sum(ua.weight)) as score").
+      select("candidates.id, parties.symbol as symbol, name, nickname, round(sum((
+             CASE 
+             WHEN answers.short_answer = ua.short_answer 
+              THEN 100 
+             WHEN ua.short_answer IS NULL 
+              THEN NULL ELSE 0 END)::numeric * ua.weight)::numeric / sum(ua.weight)) as score").
       joins(:answers).
       joins(:party).
       joins("LEFT JOIN answers ua ON ua.question_id = answers.question_id AND ua.responder_type = 'User'").
       where("ua.responder_id = ?", user_id)
     if options[:party_id] then candidates = candidates.where(:party_id => options[:party_id]) end
     if options[:union_id] then candidates = candidates.where("parties.union_id = ?", options[:union_id]) end
-    connection.select_all(candidates.order("score DESC").group("candidates.name, candidates.id, symbol"))
+    connection.select_all(candidates.order("score DESC").group("candidates.name, candidates.nickname, candidates.id, symbol"))
   end
 
 end
