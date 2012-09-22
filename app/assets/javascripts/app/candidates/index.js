7d7238493b5761837da9fb18198af1f61a3a6965
Luiz Fonseca <runeroniek@gmail.com>
2012-09-21 23:44:26 -0300

    Add spinning bottle to indicante loading on the list of candidates

--- app/assets/javascripts/app/candidates/index.js
+++ app/assets/javascripts/app/candidates/index.js
@@ -2,15 +2,22 @@ App.Candidates.Index = Backbone.View.extend({
   el: 'body',
 
   initialize: function() {
+    this.parties  = $('.list_of_candidates .parties');
+    this.bottle   = $('.list_of_candidates .loading');
   },
 
   events: {
     'change .filters_box' : 'submitFilter',
-    'ajax:success' : 'loadCandidates'
+    'ajax:before'         : 'showSpinningBottle',
+    'ajax:success'       : 'loadCandidates'
+  },
+
+  showSpinningBottle: function() {
+    this.parties.empty().append(this.bottle.fadeIn());
   },
 
   loadCandidates: function(event, data) {
-    jQuery('.list_of_candidates .parties').fadeOut(50).empty().append(data).fadeIn('slow');
+    this.parties.fadeOut(50).empty().append(data).fadeIn('slow');
   },
 
   submitFilter: function(event){
