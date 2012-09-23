d687d2c3bfcbff6848ebb1bd9b155fbbbe6e8921
Nícolas Iensen <nicolas@engage.is>
2012-09-23 17:26:22 -0300

    Fix some tests

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -19,19 +19,22 @@ describe Candidate do
         FactoryGirl.create(:user_answer, :question => question1, :short_answer => "Sim", :responder => user)
       end
 
-      it{ should == [{"name" => candidate1.name, "score" => "100"}, {"name" => candidate2.name, "score" => "0"}] }
-    
+      it{ should == [
+        {"id" => candidate1.id.to_s, "symbol" => candidate1.party.symbol, "name" => candidate1.name, "nickname" => candidate1.nickname, "score" => "100"}, 
+        {"id" => candidate2.id.to_s, "symbol" => candidate2.party.symbol, "name" => candidate2.name, "nickname" => candidate2.nickname, "score" => "0"}
+      ]}
+
       context "when I look for candidates in a specific party" do
         subject { Candidate.match_for_user(user.id, :party_id => party1.id) }
-        it{ should == [{"name" => candidate1.name, "score" => "100"}] }
+        it{ should == [{"id" => candidate1.id.to_s, "symbol" => candidate1.party.symbol, "name" => candidate1.name, "nickname" => candidate1.nickname, "score" => "100"}] }
       end
-      
+
       context "when I look for candidates in a specific union" do
         subject { Candidate.match_for_user(user.id, :union_id => union1.id) }
-        it{ should == [{"name" => candidate1.name, "score" => "100"}] }
+        it{ should == [{"id" => candidate1.id.to_s, "symbol" => candidate1.party.symbol, "name" => candidate1.name, "nickname" => candidate1.nickname, "score" => "100"}] }
       end
     end
-    
+
     context "when I have different weights for questions" do
       before do
         FactoryGirl.create(:candidate_answer, :question => question1, :short_answer => "Sim", :responder => candidate1)
@@ -41,10 +44,12 @@ describe Candidate do
         FactoryGirl.create(:user_answer, :question => question1, :short_answer => "Sim", :responder => user, :weight => 2)
         FactoryGirl.create(:user_answer, :question => question2, :short_answer => "Não", :responder => user, :weight => 1)
       end
-      
-      it{ should == [{"name" => candidate1.name, "score" => "67"}, {"name" => candidate2.name, "score" => "33"}] }
+
+      it{ should == [
+        {"id" => candidate1.id.to_s, "symbol" => candidate1.party.symbol, "name" => candidate1.name, "nickname" => candidate1.nickname, "score" => "67"}, 
+        {"id" => candidate2.id.to_s, "symbol" => candidate2.party.symbol, "name" => candidate2.name, "nickname" => candidate2.nickname, "score" => "33"}
+      ]}
     end 
   end
 
 end
-
