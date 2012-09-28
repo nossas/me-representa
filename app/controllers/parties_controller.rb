44c6134db865c9362e65514303cc276a0718e6f4
Nícolas Iensen <nicolas@engage.is>
2012-09-28 14:58:12 -0300

    Add username to parties/index

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,9 +2,10 @@ class PartiesController < ApplicationController
   layout 'application_phase_two'
   inherit_resources
 
+  before_filter :only => [:index] { @user = User.find(params[:user_id]) }
   before_filter :only => [:index] do 
-    @matching = Party.unrelated.match_for_user(params[:user_id])
-    @matching = @matching.concat Union.match_for_user(params[:user_id])
+    @matching = Party.unrelated.match_for_user(@user.id)
+    @matching = @matching.concat Union.match_for_user(@user.id)
     @matching = @matching.sort{|x, y| y["score"].to_i <=> x["score"].to_i}
   end
 end
