f847aaf9d6b1f6f1dce43c5d0d22174b327d668b
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-08-16 12:03:58 -0300

    Expandindo forma de configuração do email

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :address        => ENV['SENDGRID_URL'], # ex: 'smtp.sendgrid.net'
+    :port           => ENV['SENDGRID_PORT'], # ex: '587'
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+    :domain         => ENV['SENDGRID_DOMAIN'] # ex: 'heroku.com'
   }
   ActionMailer::Base.delivery_method = :smtp
 end
