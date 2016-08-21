b975ff274be1fbe916d81619e92fc1eb7e9cb4e3
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-08-21 11:50:09 -0300

    Corrigindo migração do nome do campo

--- db/migrate/20160821142930_add_documents_to_candidate.rb
+++ db/migrate/20160821142930_add_documents_to_candidate.rb
@@ -1,6 +1,6 @@
 class AddDocumentsToCandidate < ActiveRecord::Migration
   def change
     add_column :candidates, :cpf, :string
-    add_column :candidates, :titulo_eleitoral, :string
+    add_column :candidates, :electoral_title, :string
   end
 end
