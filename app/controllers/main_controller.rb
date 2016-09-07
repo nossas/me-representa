24548f4191707809ab6b19066929e9d77e022168
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-09-07 12:12:58 -0300

    Corrigindo problema de login de browser viciado

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,14 +6,25 @@ class MainController < ApplicationController
     
     def index
         if session[:candidate_id]
-            candidate = Candidate.find session[:candidate_id]
-            if candidate.finished_at
-                redirect_to candidate_path(candidate.id)
+            candidate_id = session[:candidate_id]
+            if not Candidate.exists? candidate_id
+                reset_session
+                redirect_to(root_path)
             else
-                redirect_to(edit_candidate_path(candidate.id))
-            end            
+                candidate = Candidate.find candidate_id
+                if candidate.finished_at
+                    redirect_to candidate_path(candidate_id)
+                else
+                    redirect_to(edit_candidate_path(candidate_id))
+                end
+            end
         elsif session[:user_id]
-            redirect_to new_answer_path;
+            if not User.exists? session[:user_id]
+                reset_session
+                redirect_to(root_path)
+            else
+                redirect_to new_answer_path;
+            end
         end
     end
     
