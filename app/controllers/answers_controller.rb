39bb8b31a2eda7d2f233d1c786cfb79a091d40f3
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-09-10 16:52:05 -0300

    Checkbox em eleitores

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -33,35 +33,48 @@ class AnswersController < ApplicationController
   end
   
   def update
-    @answer.short_answer = params[:answer][:short_answer]
-    @answer.weight = params[:weight] if params[:weight]
     if params[:candidate_id]
-        @answer.responder = Candidate.find params[:candidate_id]
+      @answer.short_answer = params[:answer][:short_answer]
+      @answer.responder = Candidate.find params[:candidate_id]
     else
-        @answer.responder = User.find params[:user_id]
+      @answer.weight = params[:weight]
+      @answer.responder = User.find params[:user_id]
     end
-    @answer.save
-    render :json => @answer
+    if @answer.save
+      render :json => @answer
+    else
+      render :status => 500
+    end
   end
 
   def new
     if params[:candidate_id]
-        @current_candidate = Candidate.find params[:candidate_id]
-        if @current_candidate.finished_at
-            redirect_to candidate_path @candidate
-        else
-            Question.where("chosen = true and role_type = 'truth'").each{|q|
-                a = Answer.new
-                a.question = q
-                a.responder = @candidate
-                a.short_answer = 'Não'
-                a.weight = 0
-                a.save
-            }
-            render(:file => "answers/new_for_candidate")
-        end
+      @current_candidate = Candidate.find params[:candidate_id]
+      if @current_candidate.finished_at
+        redirect_to candidate_path @candidate
+      else
+        Question.where("chosen = true and role_type = 'truth'").each{|q|
+          a = Answer.new
+          a.question = q
+          a.responder = @candidate
+          a.short_answer = 'Não'
+          a.weight = 0
+          a.save
+        }
+        render(:file => "answers/new_for_candidate")
+      end
     else
-        new! { return render(:file => "answers/new_for_user") }
+      Question.where("chosen = true and role_type = 'truth'").each do |q|
+        if (not @current_user.answers) or (@current_user.answers.select{|a| a.question.id == q.id}.count == 0)
+          a = Answer.new
+          a.question = q
+          a.responder = @current_user
+          a.short_answer = 'Sim'
+          a.weight = 0
+          a.save
+        end
+      end
+      new! { return render(:file => "answers/new_for_user") }
     end
   end
 end
