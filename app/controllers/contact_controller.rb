ab848701efb3d86f10ad3b1a6809702ca30ba6d9
Luiz Alb. Silva <luizalbsilva@gmail.com>
2016-09-15 00:10:31 -0300

    Envio de emails de forma simples

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,3 +1,4 @@
+require 'net/smtp'
 
 class ContactController < ApplicationController
     layout "merepresentalogged"
@@ -7,6 +8,23 @@ class ContactController < ApplicationController
     before_filter :load_logged
 
     def create
+    	contato_login = ENV['MEREP_CONTATO_LOGIN']
+    	contato_senha = ENV['MEREP_CONTATO_PASS']
+
+    	message = <<MESSAGE_END
+From: Sistema de Contatos <testes@tamanhofamilia.com.br>
+To: <pirola@nossascidades.org>
+Subject: CONTATO
+
+* Nome: #{params[:nome]}
+* Email: #{params[:email]}
+* Cidade: #{params[:city_id]} - #{params[:city]}
+* Mensagem:
+#{params[:mensagem]}
+MESSAGE_END
+    	Net::SMTP.start('mail.tamanhofamilia.com.br', 587, 'localhost', contato_login, contato_senha) do |smtp| 
+    		smtp.send_message(message, 'testes@tamanhofamilia.com.br', 'pirola@nossascidades.org', 'testes@tamanhofamilia.com.br')
+		end
     end
     
     def show
